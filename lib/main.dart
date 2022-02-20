709dc64895bef5e76859c49b59f90c3c7b2e19d9
israr <isrargul00@gmail.com>
2022-02-21 01:46:49 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 
 void main() {
-  runApp(MaterialApp(routes: {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+      routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
     '/location': (context) => ChooseLocation()
-  }));
+      }));
 }
